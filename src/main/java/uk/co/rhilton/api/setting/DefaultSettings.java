99b43cdfc0292ca61e41545b668114d9fd2f407a
Ryan Hilton <r.t.hilton2@ncl.ac.uk>
2025-05-03 02:42:29 +0100

    Adjust default settings

--- src/main/java/uk/co/rhilton/api/setting/DefaultSettings.java
+++ src/main/java/uk/co/rhilton/api/setting/DefaultSettings.java
@@ -3,6 +3,6 @@ package uk.co.rhilton.api.setting;
 public class DefaultSettings {
 
     public static final Setting<Integer> TIME_LIMIT = Setting.Integer("time_limit", () -> 600);
-    public static final Setting<Boolean> BINARY_SEARCH = Setting.Boolean("binary_search", () -> false);
+    public static final Setting<Boolean> BINARY_SEARCH = Setting.Boolean("binary_search", () -> true);
 
 }
--- src/main/java/uk/co/rhilton/townplanning/setting/TownSettings.java
+++ src/main/java/uk/co/rhilton/townplanning/setting/TownSettings.java
@@ -4,10 +4,10 @@ import uk.co.rhilton.api.setting.Setting;
 
 public class TownSettings {
 
-    public static final Setting<Integer> SHOP_DISTANCE = Setting.Integer("shop_distance", () -> 50);
+    public static final Setting<Integer> SHOP_DISTANCE = Setting.Integer("shop_distance", () -> 70);
 
     public static final Setting<Integer> SHOP_HEIGHT = Setting.Integer("shop_height", () -> 10);
-    public static final Setting<Integer> SHOP_WIDTH = Setting.Integer("shop_width", () -> 10);
+    public static final Setting<Integer> SHOP_WIDTH = Setting.Integer("shop_width", () -> 15);
     public static final Setting<Integer> SHOP_VALUE = Setting.Integer("shop_value", () -> 10);
 
     public static final Setting<Integer> HOUSE_HEIGHT = Setting.Integer("house_height", () -> 10);
